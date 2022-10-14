e4af3d26472840c4596b868a16ba1763a11d3a0e
Xiaoyu PENG <peng.xiaoyu@gmail.com>
2022-10-14 21:47:07 +0800

    fix: 国密公钥解析BITSTRING错误 styles: use prettier for formating chore: use tencentSM v1.0.20

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const forge_code = pm.collectionVariables.get("forge_code");
 eval(forge_code);
 
+const { asn1 } = forge;
+
 /**
  * 从PEM证书中获取国密PrivateKey对象.
  *
@@ -9,144 +11,166 @@ eval(forge_code);
  * @return 国密PrivateKey对象.
  */
 function privateKeyFromPem(pem) {
-    const msg = forge.pem.decode(pem)[0];
+  const msg = forge.pem.decode(pem)[0];
 
-    if (msg.type !== 'PRIVATE KEY') {
-        throw new Error("格式非法的商户API私钥.错误的PEM类型");
-    }
+  if (msg.type !== "PRIVATE KEY") {
+    throw new Error("格式非法的商户API私钥.错误的PEM类型");
+  }
 
-    if (msg.procType && msg.procType.type === 'ENCRYPTED') {
-        throw new Error("格式非法的商户API私钥.不支持加密的PEM")
-    }
+  if (msg?.procType === "ENCRYPTED") {
+    throw new Error("格式非法的商户API私钥.不支持加密的PEM");
+  }
 
-    const asn1 = forge.asn1
-    const privateKeyValidator = {
-        // PrivateKeyInfo
-        name: 'PrivateKeyInfo',
+  const privateKeyValidator = {
+    // PrivateKeyInfo
+    name: "PrivateKeyInfo",
+    tagClass: asn1.Class.UNIVERSAL,
+    type: asn1.Type.SEQUENCE,
+    constructed: true,
+    value: [
+      {
+        // Version (INTEGER)
+        name: "PrivateKeyInfo.version",
+        tagClass: asn1.Class.UNIVERSAL,
+        type: asn1.Type.INTEGER,
+        constructed: false,
+        capture: "privateKeyVersion",
+      },
+      {
+        // privateKeyAlgorithm
+        name: "PrivateKeyInfo.privateKeyAlgorithm",
         tagClass: asn1.Class.UNIVERSAL,
         type: asn1.Type.SEQUENCE,
         constructed: true,
-        value: [{
-            // Version (INTEGER)
-            name: 'PrivateKeyInfo.version',
+        value: [
+          {
+            name: "AlgorithmIdentifier.algorithm",
             tagClass: asn1.Class.UNIVERSAL,
-            type: asn1.Type.INTEGER,
+            type: asn1.Type.OID,
             constructed: false,
-            capture: 'privateKeyVersion'
-        }, {
-            // privateKeyAlgorithm
-            name: 'PrivateKeyInfo.privateKeyAlgorithm',
+            capture: "algorithm",
+          },
+          {
+            name: "AlgorithmIdentifier.namedCurveOID",
             tagClass: asn1.Class.UNIVERSAL,
-            type: asn1.Type.SEQUENCE,
-            constructed: true,
-            value: [{
-                name: 'AlgorithmIdentifier.algorithm',
-                tagClass: asn1.Class.UNIVERSAL,
-                type: asn1.Type.OID,
-                constructed: false,
-                capture: 'algorithm'
-            }, {
-                name: 'AlgorithmIdentifier.namedCurveOID',
-                tagClass: asn1.Class.UNIVERSAL,
-                type: asn1.Type.OID,
-                constructed: false,
-                capture: 'namedCurveOid'
-            }]
-        }, {
-            // privateKeyInfo
-            name: 'PrivateKeyInfo.PrivateKey',
-            tagClass: asn1.Class.UNIVERSAL,
-            type: asn1.Type.OCTETSTRING,
+            type: asn1.Type.OID,
             constructed: false,
-            capture: 'ecPrivateKey'
-        }]
-    };
-    const ecPrivateKeyValidator = {
-        name: 'ecPrivateKeyInfo',
+            capture: "namedCurveOid",
+          },
+        ],
+      },
+      {
+        // privateKeyInfo
+        name: "PrivateKeyInfo.PrivateKey",
         tagClass: asn1.Class.UNIVERSAL,
-        type: asn1.Type.SEQUENCE,
+        type: asn1.Type.OCTETSTRING,
+        constructed: false,
+        capture: "ecPrivateKey",
+      },
+    ],
+  };
+  const ecPrivateKeyValidator = {
+    name: "ecPrivateKeyInfo",
+    tagClass: asn1.Class.UNIVERSAL,
+    type: asn1.Type.SEQUENCE,
+    constructed: true,
+    value: [
+      {
+        // Version
+        name: "Version",
+        tagClass: asn1.Class.UNIVERSAL,
+        type: asn1.Type.INTEGER,
+        constructed: false,
+        capture: "version",
+      },
+      {
+        // PrivateKey
+        name: "PrivateKey",
+        tagClass: asn1.Class.UNIVERSAL,
+        type: asn1.Type.OCTETSTRING,
+        constructed: false,
+        capture: "privateKey",
+      },
+      {
+        // NamedCurveOID, optional, tag:0
+        name: "NamedCurveOID",
+        tagClass: asn1.Class.CONTEXT_SPECIFIC,
+        type: 0,
+        optional: true,
         constructed: true,
-        value: [{
-            // Version
-            name: 'Version',
+        value: [
+          {
+            name: "ObjectIdentifier",
             tagClass: asn1.Class.UNIVERSAL,
-            type: asn1.Type.INTEGER,
+            type: asn1.Type.OID,
             constructed: false,
-            capture: 'version'
-        }, {
-            // PrivateKey
-            name: 'PrivateKey',
+            capture: "namedCurveOid",
+          },
+        ],
+      },
+      {
+        // PublicKey, optional, tag:1
+        name: "PublicKey",
+        tagClass: asn1.Class.CONTEXT_SPECIFIC,
+        type: 1,
+        optional: true,
+        constructed: true,
+        value: [
+          {
+            name: "PublicKey",
             tagClass: asn1.Class.UNIVERSAL,
-            type: asn1.Type.OCTETSTRING,
+            type: asn1.Type.BITSTRING,
             constructed: false,
-            capture: 'privateKey'
-        }, {
-            // NamedCurveOID, optional, tag:0
-            name: 'NamedCurveOID',
-            tagClass: asn1.Class.CONTEXT_SPECIFIC,
-            type: 0,
-            optional: true,
-            constructed: true,
-            value: [{
-                name: 'ObjectIdentifier',
-                tagClass: asn1.Class.UNIVERSAL,
-                type: asn1.Type.OID,
-                constructed: false,
-                capture: 'namedCurveOid'
-            }]
+            captureBitStringValue: "publicKey",
+          },
+        ],
+      },
+    ],
+  };
 
-        }, {
-            // PublicKey, optional, tag:1
-            name: 'PublicKey',
-            tagClass: asn1.Class.CONTEXT_SPECIFIC,
-            type: 1,
-            optional: true,
-            constructed: true,
-            value: [{
-                name: 'PublicKey',
-                tagClass: asn1.Class.UNIVERSAL,
-                type: asn1.Type.BITSTRING,
-                constructed: false,
-                capture: 'publicKey'
-            }]
-        }]
-    };
+  let obj = asn1.fromDer(msg.body);
+  // get PrivateKeyInfo
+  let privateKeyInfo = {};
+  let errors = [];
+  if (asn1.validate(obj, privateKeyValidator, privateKeyInfo, errors)) {
+    obj = asn1.fromDer(forge.util.createBuffer(privateKeyInfo.ecPrivateKey));
+  } else {
+    let error = new Error("读取商户API私钥失败. ASN.1结构中没有包含私钥");
+    error.errors = errors;
+    throw error;
+  }
+  privateKeyInfo.privateKeyVersion = forge.util.bytesToHex(
+    privateKeyInfo.privateKeyVersion
+  );
+  privateKeyInfo.algorithm = asn1.derToOid(privateKeyInfo.algorithm);
+  privateKeyInfo.namedCurveOid = asn1.derToOid(privateKeyInfo.namedCurveOid);
 
-    let obj = forge.asn1.fromDer(msg.body);
-    // get PrivateKeyInfo
-    let privateKeyInfo = {}
-    let errors = [];
-    if (asn1.validate(obj, privateKeyValidator, privateKeyInfo, errors)) {
-        obj = asn1.fromDer(forge.util.createBuffer(privateKeyInfo.ecPrivateKey));
-    } else {
-        let error = new Error("读取商户API私钥失败. ASN.1结构中没有包含私钥");
-        error.errors = errors;
-        throw error;
-    }
-    privateKeyInfo.privateKeyVersion = forge.util.bytesToHex(privateKeyInfo.privateKeyVersion);
-    privateKeyInfo.algorithm = asn1.derToOid(privateKeyInfo.algorithm);
-    privateKeyInfo.namedCurveOid = asn1.derToOid(privateKeyInfo.namedCurveOid);
+  // get ECDSA PrivateKey
+  let ecPrivateKeyInfo = {};
+  errors = [];
+  if (!asn1.validate(obj, ecPrivateKeyValidator, ecPrivateKeyInfo, errors)) {
+    let error = new Error("读取商户API私钥失败. ASN.1结构中没有包含ECDSA私钥");
+    error.errors = errors;
+    throw error;
+  }
 
-    // get ECDSA PrivateKey
-    let ecPrivateKeyInfo = {}
-    errors = [];
-    if (!asn1.validate(obj, ecPrivateKeyValidator, ecPrivateKeyInfo, errors)) {
-        let error = new Error("读取商户API私钥失败. ASN.1结构中没有包含ECDSA私钥");
-        error.errors = errors;
-        throw error;
-    }
+  ecPrivateKeyInfo.version = forge.util.bytesToHex(ecPrivateKeyInfo.version);
+  ecPrivateKeyInfo.privateKey = forge.util.bytesToHex(
+    ecPrivateKeyInfo.privateKey
+  );
+  if ("publicKey" in ecPrivateKeyInfo) {
+    ecPrivateKeyInfo.publicKey = forge.util.bytesToHex(
+      ecPrivateKeyInfo.publicKey
+    );
+  }
+  if ("namedCurveOid" in ecPrivateKeyInfo) {
+    ecPrivateKeyInfo.namedCurveOid = asn1.derToOid(
+      ecPrivateKeyInfo.namedCurveOid
+    );
+  }
+  privateKeyInfo.ecPrivateKey = ecPrivateKeyInfo;
 
-    ecPrivateKeyInfo.version = forge.util.bytesToHex(ecPrivateKeyInfo.version);
-    ecPrivateKeyInfo.privateKey = forge.util.bytesToHex(ecPrivateKeyInfo.privateKey);
-    if ('publicKey' in ecPrivateKeyInfo) {
-        ecPrivateKeyInfo.publicKey = forge.util.bytesToHex(ecPrivateKeyInfo.publicKey);
-    }
-    if ('namedCurveOid' in ecPrivateKeyInfo) {
-        ecPrivateKeyInfo.namedCurveOid = asn1.derToOid(ecPrivateKeyInfo.namedCurveOid);
-    }
-    privateKeyInfo.ecPrivateKey = ecPrivateKeyInfo;
-
-    return privateKeyInfo;
+  return privateKeyInfo;
 }
 
 /**
@@ -157,143 +181,159 @@ function privateKeyFromPem(pem) {
  * @return 国密PublicKey对象.
  */
 function publicKeyFromPem(pem) {
-    let msg = forge.pem.decode(pem)[0];
+  let msg = forge.pem.decode(pem)[0];
 
-    if (msg.type !== 'PUBLIC KEY') {
-        throw new Error("格式非法的商户API公钥.错误的PEM类型");
-    }
+  if (msg.type !== "PUBLIC KEY") {
+    throw new Error("格式非法的商户API公钥.错误的PEM类型");
+  }
 
-    let obj = forge.asn1.fromDer(msg.body);
+  let obj = asn1.fromDer(msg.body);
 
-    const asn1 = forge.asn1
-    const publicKeyValidator = {
-        // pkixPublicKey
-        name: 'PublicKeyInfo',
+  const publicKeyValidator = {
+    // pkixPublicKey
+    name: "PublicKeyInfo",
+    tagClass: asn1.Class.UNIVERSAL,
+    type: asn1.Type.SEQUENCE,
+    constructed: true,
+    value: [
+      {
+        name: "AlgorithmIdentifier",
         tagClass: asn1.Class.UNIVERSAL,
         type: asn1.Type.SEQUENCE,
         constructed: true,
-        value: [{
-            name: 'AlgorithmIdentifier',
+        value: [
+          {
+            name: "AlgorithmIdentifier.algorithm",
             tagClass: asn1.Class.UNIVERSAL,
-            type: asn1.Type.SEQUENCE,
-            constructed: true,
-            value: [{
-                name: 'AlgorithmIdentifier.algorithm',
-                tagClass: asn1.Class.UNIVERSAL,
-                type: asn1.Type.OID,
-                constructed: false,
-                capture: 'algorithm'
-            }, {
-                name: 'NamedCurveOID',
-                tagClass: asn1.Class.UNIVERSAL,
-                type: asn1.Type.OID,
-                constructed: false,
-                capture: 'namedCurveOid'
-            }]
-        }, {
-            // BitString
-            name: 'PublicKey',
-            tagClass: asn1.Class.UNIVERSAL,
-            type: asn1.Type.BITSTRING,
+            type: asn1.Type.OID,
             constructed: false,
-            capture: 'publicKey'
-        }]
-    }
+            capture: "algorithm",
+          },
+          {
+            name: "NamedCurveOID",
+            tagClass: asn1.Class.UNIVERSAL,
+            type: asn1.Type.OID,
+            constructed: false,
+            capture: "namedCurveOid",
+          },
+        ],
+      },
+      {
+        // BitString
+        name: "PublicKey",
+        tagClass: asn1.Class.UNIVERSAL,
+        type: asn1.Type.BITSTRING,
+        constructed: false,
+        captureBitStringValue: "publicKey",
+      },
+    ],
+  };
 
-    // get PublicKeyInfo
-    let publicKeyInfo = {}
-    let errors = [];
-    if (!asn1.validate(obj, publicKeyValidator, publicKeyInfo, errors)) {
-        let error = new Error("读取商户API公钥失败. ASN.1结构中没有包含公钥");
-        error.errors = errors;
-        throw error;
-    }
-    publicKeyInfo.algorithm = asn1.derToOid(publicKeyInfo.algorithm);
-    publicKeyInfo.namedCurveOid = asn1.derToOid(publicKeyInfo.namedCurveOid);
-    publicKeyInfo.publicKey = forge.util.bytesToHex(publicKeyInfo.publicKey);
+  // get PublicKeyInfo
+  let publicKeyInfo = {};
+  let errors = [];
+  if (!asn1.validate(obj, publicKeyValidator, publicKeyInfo, errors)) {
+    let error = new Error("读取商户API公钥失败. ASN.1结构中没有包含公钥");
+    error.errors = errors;
+    throw error;
+  }
+  publicKeyInfo.algorithm = asn1.derToOid(publicKeyInfo.algorithm);
+  publicKeyInfo.namedCurveOid = asn1.derToOid(publicKeyInfo.namedCurveOid);
+  publicKeyInfo.publicKey = forge.util.bytesToHex(publicKeyInfo.publicKey);
 
-    return publicKeyInfo;
+  return publicKeyInfo;
 }
 
-const nonceStr = pm.variables.replaceIn('{{$guid}}'),
-    timeStamp = pm.variables.replaceIn('{{$timestamp}}');
+const nonceStr = pm.variables.replaceIn("{{$guid}}"),
+  timeStamp = pm.variables.replaceIn("{{$timestamp}}");
 
 // 变量替换，把请求中所有的Variables赋值
-const sdk = require('postman-collection'),
-    newRequest = new sdk.Request(pm.request.toJSON()),
-    resolvedRequest = newRequest.toObjectResolved(null, [pm.variables.toObject()], { ignoreOwnVariables: true }),
-    url = new sdk.Url(resolvedRequest.url),
-    canonicalUrl = url.getPathWithQuery();
+const sdk = require("postman-collection"),
+  newRequest = new sdk.Request(pm.request.toJSON()),
+  resolvedRequest = newRequest.toObjectResolved(
+    null,
+    [pm.variables.toObject()],
+    { ignoreOwnVariables: true }
+  ),
+  url = new sdk.Url(resolvedRequest.url),
+  canonicalUrl = url.getPathWithQuery();
 
 const method = request.method;
 
 let body = "";
-if (method === 'POST' || method === 'PUT' || method === 'PATCH') {
-    // 使用变量替换后的body
-    body = resolvedRequest.body.raw;
-    if (canonicalUrl.endsWith('upload')) {
-        const result = JSON.parse(JSON.stringify(resolvedRequest.body.formdata));
-        for (const [key, value] of result) {
-            if (key === 'meta') {
-                body = value;
-            }
-        }
+if (method === "POST" || method === "PUT" || method === "PATCH") {
+  // 使用变量替换后的body
+  body = resolvedRequest.body.raw;
+  if (canonicalUrl.endsWith("upload")) {
+    const result = JSON.parse(JSON.stringify(resolvedRequest.body.formdata));
+    for (const [key, value] of result) {
+      if (key === "meta") {
+        body = value;
+      }
     }
+  }
 }
 
 // 计算签名源串
-const message = method + "\n"
-    + canonicalUrl + "\n"
-    + timeStamp + "\n"
-    + nonceStr + "\n"
-    + body + "\n";
+const message =
+  method +
+  "\n" +
+  canonicalUrl +
+  "\n" +
+  timeStamp +
+  "\n" +
+  nonceStr +
+  "\n" +
+  body +
+  "\n";
 
 let auth;
 const enableShangMi = pm.environment.get("shangmi");
 if (enableShangMi == "true") {
-    console.log("using ShangMi for signature");
+  console.log("using ShangMi for signature");
 
-    const sm2js_code = pm.collectionVariables.get("sm2js_code");
-    const module = {};
-    const SM2Lib = eval(sm2js_code);
+  const sm2js_code = pm.collectionVariables.get("sm2js_code");
+  eval(sm2js_code);
 
-    const mchid = pm.environment.get("merchantId");
-    const serialNumber = pm.environment.get("merchantSerialNo");
-    // 二进制的私钥和公钥
-    const privateKeyPem = pm.environment.get("merchantPrivateKey");
-    const publicKeyPem = pm.environment.get("merchantPublicKey");
+  const mchid = pm.environment.get("merchantId");
+  const serialNumber = pm.environment.get("merchantSerialNo");
+  // 二进制的私钥和公钥
+  const privateKeyPem = pm.environment.get("merchantPrivateKey");
+  const publicKeyPem = pm.environment.get("merchantPublicKey");
 
-    const privateKeyInfo = privateKeyFromPem(privateKeyPem);
-    const publicKey = ('publicKey' in privateKeyInfo.ecPrivateKey)
-        ? privateKeyInfo.ecPrivateKey.publicKey
-        : publicKeyFromPem(publicKeyPem).publicKey;
+  const privateKeyInfo = privateKeyFromPem(privateKeyPem);
+  const publicKey =
+    "publicKey" in privateKeyInfo.ecPrivateKey
+      ? privateKeyInfo.ecPrivateKey.publicKey
+      : publicKeyFromPem(publicKeyPem).publicKey;
 
-    const sm2 = new SM2Lib.SM2;
-    const utils = SM2Lib.utils;
-    const messageByteArray = utils.stringToByteArrayInUtf8(message);
-    const idByteArray = utils.stringToByteArrayInUtf8('1234567812345678');
-    const signHex = sm2.sign(publicKey, privateKeyInfo.ecPrivateKey.privateKey, messageByteArray, idByteArray);
-    const signature = utils.byteArrayToBase64(utils.hexToByteArray(signHex));
+  const signHex = new SM2Lib.SM2().sign(
+    publicKey,
+    privateKeyInfo.ecPrivateKey.privateKey,
+    SM2Lib.utils.stringToByteArrayInUtf8(message),
+    SM2Lib.utils.stringToByteArrayInUtf8("1234567812345678")
+  );
 
-    auth = `WECHATPAY2-SM2-WITH-SM3 mchid="${mchid}",serial_no="${serialNumber}",nonce_str="${nonceStr}",timestamp="${timeStamp}",signature="${signature}"`;
+  const signature = forge.util.encode64(forge.util.hexToBytes(signHex));
+  auth = `WECHATPAY2-SM2-WITH-SM3 mchid="${mchid}",serial_no="${serialNumber}",nonce_str="${nonceStr}",timestamp="${timeStamp}",signature="${signature}"`;
 } else {
-    console.log("using RSA for signature");
+  console.log("using RSA for signature");
 
-    const mchid = pm.environment.get("merchantId");
-    const serialNo = pm.environment.get("merchantSerialNo");
-    // pem私钥字符串
-    const privateKeyStr = pm.environment.get("merchantPrivateKey");
+  const mchid = pm.environment.get("merchantId");
+  const serialNo = pm.environment.get("merchantSerialNo");
+  // pem私钥字符串
+  const privateKeyStr = pm.environment.get("merchantPrivateKey");
+  // 从pem中加载私钥
+  const privateKey = forge.pki.privateKeyFromPem(privateKeyStr);
 
-    // 从pem中加载私钥
-    const privateKey = forge.pki.privateKeyFromPem(privateKeyStr);
-    const sha256 = forge.md.sha256.create();
-    sha256.update(forge.util.encodeUtf8(message));
-    const signature = forge.util.encode64(privateKey.sign(sha256));
+  const sha256 = forge.md.sha256.create();
+  sha256.update(forge.util.encodeUtf8(message));
 
-    auth = `WECHATPAY2-SHA256-RSA2048 mchid="${mchid}",serial_no="${serialNo}",nonce_str="${nonceStr}",timestamp="${timeStamp}",signature="${signature}"`;
+  const signature = forge.util.encode64(privateKey.sign(sha256));
+  auth = `WECHATPAY2-SHA256-RSA2048 mchid="${mchid}",serial_no="${serialNo}",nonce_str="${nonceStr}",timestamp="${timeStamp}",signature="${signature}"`;
 }
 
 pm.request.headers.add({
-    key: "Authorization",
-    value: auth
+  key: "Authorization",
+  value: auth,
 });
